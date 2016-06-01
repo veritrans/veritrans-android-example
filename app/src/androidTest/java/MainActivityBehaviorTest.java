0ebd93eca43fc71700319dc72e2b97a3670bb24a
rakawm <rakamogandhi@hotmail.com>
2016-06-01 15:24:39 +0700

    Add automated instrumentation test.
    Revise the test case.

--- app/src/androidTest/java/MainActivityBehaviorTest.java
+++ app/src/androidTest/java/MainActivityBehaviorTest.java
@@ -5,6 +5,7 @@ import android.support.test.espresso.web.webdriver.Locator;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -15,6 +16,7 @@ import id.co.veritrans.sdk.sample.MainActivity;
 import id.co.veritrans.sdk.sample.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -41,6 +43,8 @@ public class MainActivityBehaviorTest {
     String cardNo, cardCVV, cardExpired;
     String cardNoExpected, cardCVVExcpected, cardExpiredExpected;
     String email, emailExpected;
+    private String fullName, phone;
+    private String address, city, zipcode, country;
 
     @Before
     public void initString() {
@@ -50,14 +54,55 @@ public class MainActivityBehaviorTest {
         cardCVVExcpected = "123";
         cardExpired = "0120";
         cardExpiredExpected = "01/20";
-        email = "sreekanth@veritrans.co.id";
-        emailExpected = "sreekanth@veritrans.co.id";
+
+        fullName = "Raka Westu Mogandhi";
+        email = "westumogandhi@gmail.com";
+        emailExpected = "westumogandhi@gmail.com";
+        phone = "082140518011";
+        address = "Jalan Wirajaya 312";
+        city = "Yogyakarta";
+        zipcode = "55198";
+        country = "Indonesia";
+
+        runPrologue();
+    }
+
+    public void runPrologue() {
+        if (mActivityRule.getActivity().findViewById(R.id.btn_get_token).getVisibility() == View.VISIBLE) {
+            // Click get authentication token first
+            onView(withId(R.id.btn_get_token)).perform(click());
+
+            // Wait until get token finished
+            SystemClock.sleep(2000);
+            // Go to credit card
+            onView(withId(R.id.btn_credit_ui)).perform(scrollTo(), click());
+
+            // Fill consumer name
+            onView(withId(R.id.et_full_name)).perform(clearText(), typeText(fullName), closeSoftKeyboard());
+            onView(withId(R.id.et_email)).perform(clearText(), typeText(email), closeSoftKeyboard());
+            onView(withId(R.id.et_phone)).perform(clearText(), typeText(phone), closeSoftKeyboard());
+
+            // Click next button
+            onView(withId(R.id.btn_next)).perform(click());
+
+            // Fill consumer details
+            onView(withId(R.id.et_address)).perform(clearText(), typeText(address), closeSoftKeyboard());
+            onView(withId(R.id.et_city)).perform(clearText(), typeText(city), closeSoftKeyboard());
+            onView(withId(R.id.et_zipcode)).perform(clearText(), typeText(zipcode), closeSoftKeyboard());
+            onView(withId(R.id.et_country)).perform(clearText(), typeText(country), closeSoftKeyboard());
+
+            // Click next button
+            onView(withId(R.id.btn_next)).perform(click());
+
+            pressBack();
+        }
     }
 
     @Test
     public void creditCardFlowTest() {
         // Initializing credit card payment
-        onView(ViewMatchers.withId(R.id.btn_credit_ui)).perform(click());
+        onView(withId(R.id.radio_card_two_click)).perform(scrollTo(), click());
+        onView(ViewMatchers.withId(R.id.btn_credit_ui)).perform(scrollTo(), click());
         onView(withId(R.id.rv_payment_methods)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         // Fill credit card data
